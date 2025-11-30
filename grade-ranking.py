10b0e3a531c3016c95067073cd42a7122ef744ed
JuneChen1 <june850713@gmail.com>
2025-11-30 06:19:59 +0000

    user can input more or less than 10 grades

--- grade-ranking.py
+++ grade-ranking.py
@@ -1,10 +1,23 @@
 gradeList = []
+i = 1
 
-for i in range(1, 11):
-  grade = int(input("請輸入第"+str(i)+"個成績："))
-  gradeList.append(grade)
+print("--請至少輸入6個成績，輸入任意字母可進行結算--")
+
+while True:
+  grade = input("請輸入第"+str(i)+"個成績：")
+
+  if grade.isdigit() == False:
+    if len(gradeList) < 6:
+      print("--至少輸入6個成績，請重新輸入--")
+      grade = input("請輸入第"+str(i)+"個成績：")
+    else:
+      break
+  
+  i += 1
+  gradeList.append(int(grade))
 
 gradeList.sort()
+length = len(gradeList)
 
-print("最大的三個成績：{}、{}、{}".format(gradeList[9], gradeList[8], gradeList[7]))
+print("最大的三個成績：{}、{}、{}".format(gradeList[length-1], gradeList[length-2], gradeList[length-3]))
 print("最小的三個成績：{}、{}、{}".format(gradeList[0], gradeList[1], gradeList[2]))
