6874915f753677e6b6b3f66028d57e5d06897304
JuneChen1 <june850713@gmail.com>
2025-12-07 12:45:36 +0000

    user can only input number or English word

--- grade-ranking.py
+++ grade-ranking.py
@@ -5,14 +5,15 @@ print("--請至少輸入6個成績，輸入任意字母可進行結算--")
 
 while True:
   grade = input("請輸入第"+str(i)+"個成績：")
-
-  if grade.isdigit() == False:
+  if grade.encode().isalpha() == True:
     if len(gradeList) < 6:
-      print("--至少輸入6個成績，請重新輸入--")
-      grade = input("請輸入第"+str(i)+"個成績：")
+      print("------請至少輸入6個成績------")
+      continue
     else:
       break
-  
+  elif grade.isdigit() == False:
+    print("----請輸入正整數，或輸入任意字母進行結算----")
+    continue
   i += 1
   gradeList.append(int(grade))
 
@@ -22,5 +23,5 @@ average = round(sum(gradeList)/length, 1)
 
 print("----------------------")
 print("平均成績：", average)
-print("最大的三個成績：{}、{}、{}".format(gradeList[length-1], gradeList[length-2], gradeList[length-3]))
+print("最大的三個成績：{}、{}、{}".format(gradeList[-1], gradeList[-2], gradeList[-3]))
 print("最小的三個成績：{}、{}、{}".format(gradeList[0], gradeList[1], gradeList[2]))
